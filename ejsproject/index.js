6e034a5d4e49955e365a429c84709edfb771b88c
Abhishek-A <kua77663@gmail.com>
2024-07-03 18:47:33 +0530

    some error resolve when we create some tasks that not shown up after openup that error reolve now its work fine

--- ejsproject/index.js
+++ ejsproject/index.js
@@ -14,6 +14,15 @@ app.get("/", function (req, res) {
     res.render("index",{files: files});
   });
 });
+app.get("/files/:filename", function (req, res) {
+  const filename = req.params.filename;
+  fs.readFile(`./files/${filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.render('show', { filename: req.params.filename, filedata: filedata});
+  });
+});
 app.post("/create", function (req, res) {
   fs.writeFile(`./files/${req.body.title.split(' ').join(' ')}.txt`, req.body.details, function(err) {
     res.redirect("/")
