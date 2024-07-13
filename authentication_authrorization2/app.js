381c8b5d8e264a080c65583b3fa952643330ee9d
Abhishek-A <kua77663@gmail.com>
2024-07-13 21:56:15 +0530

    created a simple ui that take 4 details username,email,password and age and create a simple id in which we hash the password by the help of bcrypt

--- authentication_authrorization2/app.js
+++ authentication_authrorization2/app.js
@@ -4,6 +4,9 @@ const app = express();
 const path = require("path");
 const cokkieparser = require("cookie-parser");
 const userModel = require("./models/user");
+const bcrypt = require('bcrypt');
+const { hash } = require("crypto");
+
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -13,6 +16,22 @@ app.use(cookieParser());
 
 app.get("/", function (req, res) {
   res.render("index");
+});
+  
+app.post('/create', async (req,res) =>{
+  let {username , email, password, age} = req.body;
+  
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async(err, hash) =>{
+      let createdUser= await userModel.create({
+        username,
+        email,
+        password: hash,
+        age
+      })
+      res.send(createdUser);
+    })
+  })
 });
 
 app.listen(3000);
